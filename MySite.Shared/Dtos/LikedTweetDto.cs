581a389f8baf833b07d08d903a63d706169a1f5d
Sean Edwards <hello@seanedwards.dev>
2024-06-19 17:53:56 +0100

    Make values nullable

--- MySite.Shared/Dtos/LikedTweetDto.cs
+++ MySite.Shared/Dtos/LikedTweetDto.cs
@@ -2,7 +2,7 @@ namespace MySite.Shared.Dtos;
 
 public record LikedTweetDto
 {
-    public string TweetLink { get; set; } = string.Empty;
+    public string? TweetLink { get; set; }
     public byte[]? Screenshot { get; set; }
-    public DateTime LikedDate { get; set; }
+    public DateTime? LikedDate { get; set; }
 }
