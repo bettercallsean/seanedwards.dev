2c35c237b11e67831ab64a452cce77f2b6ae0540
Sean Edwards <hello@seanedwards.dev>
2024-11-17 19:25:13 +0000

    Move publishing code to global function

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -1,13 +1,11 @@
 $scriptDirectory = Get-Location
 
 Set-Location "MySite"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-Push-Container("mysite")
+Publish-Container "mysite"
 
 Set-Location $scriptDirectory
 
 Set-Location "MySite.API"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-Push-Container("mysite-api")
+Publish-Container "mysite-api"
 
 ssh pi 'cd ~/MySite && docker compose pull && docker-compose up -d --no-build'
