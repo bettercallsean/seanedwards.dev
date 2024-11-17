41c0045d79938798024751d897045abd66b89bd6
Sean Edwards <hello@seanedwards.dev>
2024-11-17 18:16:55 +0000

    Use custom Push-Container function to push containers

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -2,14 +2,12 @@ $scriptDirectory = Get-Location
 
 Set-Location "MySite"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-docker tag mysite raspberrypi.local:9298/mysite
-docker push raspberrypi.local:9298/mysite
+Push-Container("mysite")
 
 Set-Location $scriptDirectory
 
 Set-Location "MySite.API"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-docker tag mysite-api raspberrypi.local:9298/mysite-api
-docker push raspberrypi.local:9298/mysite-api
+Push-Container("mysite-api")
 
 ssh pi 'cd ~/MySite && docker compose pull && docker-compose up -d --no-build'
