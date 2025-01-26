258662b23d35243e875b4f75fa7329dd26a45508
Sean Edwards <hello@seanedwards.dev>
2025-01-26 12:54:16 +0000

    Remove --no-build argument

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -8,6 +8,6 @@ Set-Location $scriptDirectory
 Set-Location "MySite.API"
 Publish-Container "mysite-api"
 
-ssh $env:SERVER_NAME 'cd ~/MySite && docker compose pull && docker compose up -d --no-build'
+ssh $env:SERVER_NAME 'cd ~/MySite && docker compose pull && docker compose up -d'
 
 Set-Location $scriptDirectory
